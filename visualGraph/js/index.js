29f4c31aff9e769f6d0833b20f0f8963dc422756
simeon <simeon@sz-development.com>
2020-05-06 14:35:58 +0200

    version 1 - visualizing a simple 2d path

--- visualGraph/js/index.js
+++ visualGraph/js/index.js
@@ -1,16 +1,35 @@
-// Graph Visualizer
-var speed = 0.2;
-var PI = Math.PI;
-var x = 0;
-var y = 0;
-function setY(x) {
+// User interface
+const UI_container = document.querySelector(".box");
+const UI_circle = document.querySelector(".circle");
+// constants
+const getYCoordinate = (x) => {
     return Math.sin(2 * x * 0.04) * -1;
+};
+class PathVisualizer {
+    constructor() {
+        // Class constants
+        this.#BOX_WIDTH = UI_container.getBoundingClientRect().width;
+        this.#PI = Math.PI;
+        this.#FACTOR = this.#BOX_WIDTH / (this.#PI * 25);
+        this.#Y_FACTOR = 10;
+        this.#INTERVALL = 100;
+    }
+    // Class constants
+    #BOX_WIDTH;
+    #PI;
+    #FACTOR;
+    #Y_FACTOR;
+    #INTERVALL;
+    visualizePath(i) {
+        setTimeout(() => {
+            let yCoord = getYCoordinate(i * this.#PI);
+            let xCoord = i * this.#PI * this.#FACTOR;
+            UI_circle.style.bottom = `${yCoord * this.#Y_FACTOR}px`;
+            UI_circle.style.left = `${xCoord}px`;
+        }, this.#INTERVALL * i);
+    }
 }
-for (var i = 0; i <= 25; i++) {
-    y = setY(i * PI);
-    console.log(y);
+const pathVisualizer = new PathVisualizer();
+for (let i = 0; i <= 1; i++) {
+    pathVisualizer.visualizePath(i);
 }
-// get element length
-var elemWidth = document.querySelector(".element").getBoundingClientRect();
-console.log(elemWidth.width);
-console.log(elemWidth.width / (Math.PI * 25));
