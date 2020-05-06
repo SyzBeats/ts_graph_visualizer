a75851aa4b87188baaf7015be5eb7c72d6845afd
Simeon Zimmermann <32411090+SyzBeats@users.noreply.github.com>
2020-05-06 14:31:55 +0200

    Update index.ts

--- visualGraph/ts/index.ts
+++ visualGraph/ts/index.ts
@@ -1,23 +1,37 @@
-// Graph Visualizer
+// User interface
 
-const speed: number = 0.2;
-const PI = Math.PI;
+const UI_container = document.querySelector<HTMLDivElement>(".box");
+const UI_circle = document.querySelector<HTMLDivElement>(".circle");
 
-let x: number = 0;
-let y: number = 0;
+// constants
 
-function setY(x) {
-    return Math.sin(2 * x * 0.04) * -1;
+const getYCoordinate = (x: number) => {
+  return Math.sin(2 * x * 0.04) * -1;
+};
+
+class PathVisualizer {
+  // Class constants
+  #BOX_WIDTH: number = UI_container.getBoundingClientRect().width;
+  #PI: number = Math.PI;
+  #FACTOR: number = this.#BOX_WIDTH / (this.#PI * 25);
+  #Y_FACTOR: number = 10;
+  #INTERVALL: number = 100;
+
+  constructor() {}
+
+  public visualizePath(i: number) {
+    setTimeout(() => {
+      let yCoord: number = getYCoordinate(i * this.#PI);
+      let xCoord: number = i * this.#PI * this.#FACTOR;
+
+      UI_circle.style.bottom = `${yCoord * this.#Y_FACTOR}px`;
+      UI_circle.style.left = `${xCoord}px`;
+    }, this.#INTERVALL * i);
+  }
 }
 
-for (let i = 0; i <= 25; i++) {
-    y = setY(i * PI);
-    console.log(y);
+const pathVisualizer = new PathVisualizer();
+
+for (let i = 0; i <= 1; i++) {
+  pathVisualizer.visualizePath(i);
 }
-
-// get element length
-
-let elemWidth = document.querySelector(".element").getBoundingClientRect();
-console.log(elemWidth.width);
-
-console.log(elemWidth.width / (Math.PI * 25));
